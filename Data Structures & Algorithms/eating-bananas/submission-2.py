4c5c45b5ccd3953b0e076423b78e8f8d7439c3ae
Lien Pham <79825085+lienpham2512@users.noreply.github.com>
2026-06-05 17:59:59 -0500

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        '''
+        binary search [1, max(piles)]
+        validate: deduct ceil(pile/k) from h, invalid if h < 0
+        if valid, search lower bound, if not, search upper bound until valid
+        '''
+
+        l, r = 1, max(piles)
+        k = r
+        while l <= r:
+            mid = l + (r-l)//2
+            hour = h
+            for p in piles:
+                hour -= math.ceil(p/mid)
+            if hour < 0:
+                l = mid + 1
+            else:
+                r = mid - 1
+                k = min(mid, k)
+        return k
